960583637cd412887eae21307c72b00acccde889
Neepsy <eric55664@gmail.com>
2020-07-04 02:14:55 -0500

    HTML page to display data
    The server now runs a second websocket that sends data to a HTML page.
    Use run.bat to quickly start up all the programs.

--- server.py
+++ server.py
@@ -1,13 +1,37 @@
 import websockets
 import asyncio
+import queue
+
+lastFiveNums = []
+
+
+def addtolist(data):
+    if len(lastFiveNums) >= 5:
+        lastFiveNums.pop(0)
+    lastFiveNums.append(data)
+    print(lastFiveNums)
+
 
 async def send(websocket, path):
     data = await websocket.recv()
     print("Counter sent " + data)
+    addtolist(data)
 
-    reply = "Confirming recieved data was " + data
+    reply = data
     await websocket.send(reply)
 
-start_server = websockets.serve(send, "localhost", 8700)
+
+async def displaydata(websocket, path):
+    while True:
+        if len(lastFiveNums) > 0:
+            tosend = lastFiveNums[len(lastFiveNums) - 1]
+            await websocket.send(tosend)
+            await asyncio.sleep(5)
+
+
+start_server = websockets.serve(send, "127.0.0.1", 8700)
+start_webpage = websockets.serve(displaydata, "127.0.0.1", 8701)
+print("Starting the server")
 asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_until_complete(start_webpage)
 asyncio.get_event_loop().run_forever()
