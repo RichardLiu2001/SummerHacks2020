bd24a8acc3c38bf0ec68a8d758f7ca49f9209455
Neepsy <eric55664@gmail.com>
2020-07-23 11:23:50 -0500

    Adding chart
    React app now has a graph that displays previous lucky numbers.

--- counter_app/server.js
+++ counter_app/server.js
@@ -11,14 +11,14 @@ wss.on("connection", ws => {
     ws.on("message", message =>{
         console.log("Recieved Message: '" + message + "'") 
     } )
-    ws.send("Hello there")
+    ws.send("0000")
     connections.push(ws)
 })
 
 //broadcast a number to every websocket in the array
 function broadcast(){
     randomNum = Math.floor(Math.random() * 8999 + 1000)
-    
+    console.log("Connections: " + connections.length);
     for(i = 0 ; i < connections.length; i++){
         connections[i].send(randomNum);
     }
